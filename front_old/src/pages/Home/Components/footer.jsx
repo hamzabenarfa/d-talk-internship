a77d6238d0d72902943cd49b044faecef8ad7fb3
Hamza Benarfa <hamzabenarfa4@gmail.com>
2025-02-02 01:37:48 +0100

    deleted old front

--- front_old/src/pages/Home/Components/footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <footer className="flex items-center justify-center text-sm relative bottom-0">
-            <p>© 2024, All rights reserved </p>
-        </footer>
-     );
-}
- 
-export default Footer;
