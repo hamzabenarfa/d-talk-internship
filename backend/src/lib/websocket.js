a731c342fbb202a60b1775ae42bda121eba5f339
hamza benarfa <hamzabenarfa4@gmail.com>
2025-06-17 18:02:32 +0100

    refactor: improve formatting and readability of WebSocketServer class

--- backend/src/lib/websocket.js
+++ backend/src/lib/websocket.js
@@ -1,36 +1,39 @@
 const { WebSocket, WebSocketServer: WSS } = require("ws");
 
 class WebSocketServer {
-    static instance;
+  static instance;
 
-    constructor() {
-        if (WebSocketServer.instance) {
-            return WebSocketServer.instance;
-        }
-
-        this.wss = new WSS({ port: 8081 });
-        this.wss.on("connection", (ws) => {
-            console.log("Client connected");
-            ws.on("close", () => console.log("Client disconnected"));
-        });
-
-        WebSocketServer.instance = this;
+  constructor() {
+    if (WebSocketServer.instance) {
+      return WebSocketServer.instance;
     }
 
-    static getInstance() {
-        if (!WebSocketServer.instance) {
-            WebSocketServer.instance = new WebSocketServer();
-        }
-        return WebSocketServer.instance;
-    }
+    this.wss = new WSS({ port: 8081 });
+    this.wss.on("connection", (ws) => {
+      console.log("Client connected");
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send("Hi");
+      }
+      ws.on("close", () => console.log("Client disconnected"));
+    });
 
-    broadcast(message) {
-        this.wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+    WebSocketServer.instance = this;
+  }
+
+  static getInstance() {
+    if (!WebSocketServer.instance) {
+      WebSocketServer.instance = new WebSocketServer();
     }
+    return WebSocketServer.instance;
+  }
+
+  broadcast(message) {
+    this.wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  }
 }
 
 const WebSocketServerInstance = WebSocketServer.getInstance();
